e60d36cd6e66cf04555fbca4debf443a98d73c1f
BigBoss <rohityou000@gmail.com>
2022-04-27 11:47:30 +0530

    Day-46

--- Day-45/test.py
+++ Day-45/test.py
@@ -9,18 +9,18 @@ soup = BeautifulSoup(contents, "lxml")
 all_anchor_tags = soup.find_all(name="a")
 all_paragraph_tags = soup.find_all(name="p")
 
-for tag in all_anchor_tags:
-    print(tag.getText("a"))
+# for tag in all_anchor_tags:
+#     print(tag.getText("a"))
 
-for tag in all_anchor_tags:
-    print(tag.get("href"))
+# for tag in all_anchor_tags:
+#     print(tag.get("href"))
 
 heading = soup.find(name="h1", id="name")
 print(heading)
 
-section_heading = soup.find(name="h3", class_= "heading")
-print(section_heading)
+# section_heading = soup.find(name="h3", class_= "heading")
+# print(section_heading)
 
-compnay_url = soup.select_one(selector="p a")
-print(compnay_url)
+# compnay_url = soup.select_one(selector="p a")
+# print(compnay_url)
 
